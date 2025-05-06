69f93d9af6eef6d014c5503b753c97ea5a6134a2
chandra <chandrasekhar1123@gmial.com>
2025-05-06 11:31:17 +0530

    hello2 commit

---
+++ src/Component/Hello2.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Hello2 extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>Hello2</div>
+    )
+  }
+}
+
+export default Hello2
